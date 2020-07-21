2c895ecfc568f218ef852cf1e52967ad27039e17
dbeccaria826 <beccdaniel@gmail.com>
2020-07-20 21:52:06 -0400

    react(addComponent): creating a better looking form

--- client/src/components/Gems/Add/Add.js
+++ client/src/components/Gems/Add/Add.js
@@ -1,71 +1,43 @@
 import React, { useState } from 'react';
-import Input from '../../Layout/Input/Input';
 import styles from './Add.css';
 
 const Add = () => {
     const [element, setElement] = useState({
-        name: {
-            elementType: 'input',
-            elementConfig: {
-                type: 'text',
-                placeholder: 'Gem Name'
-            },
-            value: ''
-        },
-        color: {
-            elementType: 'input',
-            elementConfig: {
-                type: 'text',
-                placeholder: 'Color'
-            },
-            value: ''
-        }
+        name: '',
+        color: '',
+        opacity: ''
     });
+
+    const { name, color, opacity } = element;
+
     const addGem = (e) => {
         console.log(e);
         e.preventDefault();
     };
-    const inputChangedHandler = (e, inputId) => {
-        const updateGemForm = {
-            ...element
-        };
-        const updatedFormElement = {
-            ...element[inputId]
-        };
-        updatedFormElement.value = e.target.value;
-        updateGemForm[inputId] = updatedFormElement;
 
-        setElement({ element: updateGemForm });
+    const onChange = (e) => {
+        setElement({ ...element, [e.target.name]: e.target.value });
     };
-    const formElements = [];
-    for (let key in element) {
-        console.log(key);
-        formElements.push({
-            id: key,
-            config: element[key]
-        });
-    }
-    console.log(formElements);
-    let form = (
-        <form onSubmit={addGem} className={styles.addForm}>
-            {formElements.map(formElement => (
-                <Input
-                    className={styles.input}
-                    label={formElement.config.elementConfig.placeholder}
-                    key={formElement.id}
-                    elementType={formElement.config.elementType}
-                    elementConfig={formElement.config.elementConfig}
-                    value={formElement.config.value}
-                    changed={(event) => inputChangedHandler(event, formElement.id)}
-                />
-            ))}
-        </form>
-    );
+
     console.log(typeof form);
     return (
         <div className={styles.addContent}>
             <h1>Add a Gem</h1>
-            {form}
+            <form onSubmit={addGem}>
+                <div className={styles.inputGroup}>
+                    <label htmlFor="Name">Name</label>
+                    <input type="text" name="name" value={name} onChange={onChange} />
+                </div>
+                <div className={styles.inputGroup}>
+                    <label htmlFor="Color">Color</label>
+                    <input type="text" name="color" value={color} onChange={onChange} />
+                </div>
+                <div className={styles.inputGroup}>
+                    <label htmlFor="Opacity">Opacity</label>
+                    <input type="text" name="opacity" value={opacity} onChange={onChange} />
+                </div>
+                <button className={styles.submitButton}>Submit</button>
+            </form>
         </div>
 
     );
