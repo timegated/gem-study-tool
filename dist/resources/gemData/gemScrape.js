746b0303fd449a8971ec7d20baf79992ea96fc76
dbeccaria826 <beccdaniel@gmail.com>
2020-08-10 19:03:20 -0400

    savingJSON: figuring out a way to save my json data from webscraping when there is a compilation step involved

--- dist/resources/gemData/gemScrape.js
+++ dist/resources/gemData/gemScrape.js
@@ -4,33 +4,58 @@ var _requestretry = _interopRequireDefault(require("requestretry"));
 
 var _cheerio = _interopRequireDefault(require("cheerio"));
 
+var _fs = _interopRequireDefault(require("fs"));
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-const getGemData = async () => {
-  const gems = [];
-  const gemItalics = [];
-  const req = await (0, _requestretry.default)({
-    url: 'https://www.gemdat.org/gemindex.php',
-    json: true
-  });
-
-  const $ = _cheerio.default.load(req.body);
-
-  const gemLinks = $('ul>li>a').get();
-  const gemLinksItalic = $('ul>li>i>a').get();
-  gemLinks.map(el => {
-    gems.push({
-      name: el.children[0].data,
-      link: el.href
+const getProperties = (el, arr) => {
+  return el.map(item => {
+    arr.push({
+      name: item.children[0].data
     });
   });
-  gemLinksItalic.map(el => {
-    gemItalics.push({
-      name: el.children[0].data
-    });
-  });
-  console.log(gems);
-  console.log(gemItalics);
 };
 
-getGemData();
+const getNonItalic = async () => {
+  try {
+    const gems = [];
+    const req = await (0, _requestretry.default)({
+      url: 'https://www.gemdat.org/gemindex.php',
+      json: true
+    });
+
+    const $ = _cheerio.default.load(req.body);
+
+    const gemLinks = $('ul>li>a').get();
+    getProperties(gemLinks, gems); // console.log(gems);
+
+    return gems;
+  } catch (error) {
+    console.error('Something bad happened with getNonItalic');
+  }
+};
+
+const getItalicGems = async () => {
+  try {
+    const italicGems = [];
+    const req = await (0, _requestretry.default)({
+      url: 'https://www.gemdat.org/gemindex.php',
+      json: true
+    });
+
+    const $ = _cheerio.default.load(req.body);
+
+    const gemLinksItalic = $('ul>li>i>a').get();
+    getProperties(gemLinksItalic, italicGems);
+    console.log(italicGems);
+    return italicGems;
+  } catch (error) {
+    console.error('Something bad happened with getItalicGems');
+  }
+};
+
+const saveJSON = (fileName, data) => {
+  _fs.default.writeFileSync(`${__dirname}/../data/${fileName}`, JSON.stringify(data));
+};
+
+saveJSON('gemdata_ni', getNonItalic());
--- src/resources/gemData/gemScrape.js
+++ src/resources/gemData/gemScrape.js
@@ -1,30 +1,51 @@
 import request from 'requestretry';
 import cheerio from 'cheerio';
+import fs from 'fs';
 
-const getGemData = async () => {
-  const gems = [];
-  const gemItalics = [];
-
-  const req = await request({
-    url: 'https://www.gemdat.org/gemindex.php',
-    json: true,
-  });
-  const $ = cheerio.load(req.body);
-  const gemLinks = $('ul>li>a').get();
-  const gemLinksItalic = $('ul>li>i>a').get();
-  gemLinks.map((el) => {
-    gems.push({
-      name: el.children[0].data,
-      link: el.href,
+const getProperties = (el, arr) => {
+  return el.map((item) => {
+    arr.push({
+      name: item.children[0].data,
     });
   });
-  gemLinksItalic.map((el) => {
-    gemItalics.push({
-      name: el.children[0].data,
-    });
-  });
-  console.log(gems);
-  console.log(gemItalics);
 };
 
-getGemData();
+const getNonItalic = async () => {
+  try {
+    const gems = [];
+    const req = await request({
+      url: 'https://www.gemdat.org/gemindex.php',
+      json: true,
+    });
+    const $ = cheerio.load(req.body);
+    const gemLinks = $('ul>li>a').get();
+    getProperties(gemLinks, gems);
+    // console.log(gems);
+    return gems;
+  } catch (error) {
+    console.error('Something bad happened with getNonItalic');
+  }
+};
+
+const getItalicGems = async () => {
+  try {
+    const italicGems = [];
+    const req = await request({
+      url: 'https://www.gemdat.org/gemindex.php',
+      json: true,
+    });
+    const $ = cheerio.load(req.body);
+    const gemLinksItalic = $('ul>li>i>a').get();
+    getProperties(gemLinksItalic, italicGems);
+    console.log(italicGems);
+    return italicGems;
+  } catch (error) {
+    console.error('Something bad happened with getItalicGems');
+  }
+};
+
+const saveJSON = (fileName, data) => {
+  fs.writeFileSync(`${__dirname}/../data/${fileName}`, JSON.stringify(data));
+};
+
+saveJSON('gemdata_ni', getNonItalic());
